eb586dd43b7f407769c711c2146032242ee607c5
MyAccDhika <apriandhika@gmail.com>
2025-10-18 20:12:28 +0700

    Added levels menu and fixed buttons

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -3,8 +3,8 @@ extends Control
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/level1.tscn")
 
-func _on_lebels_pressed():
-	pass # Replace with function body.
+func _on_levels_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels_menu.tscn")
 
 func _on_settings_pressed():
 	pass # Replace with function body.
